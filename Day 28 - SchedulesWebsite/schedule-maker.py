1bab1540d5adbca496f4457a9b468da70b31b0f9
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2023-01-23 21:41:54 -0500

    Added weekend support
    for SchedulesWebsite schedule-maker

--- Day 28 - SchedulesWebsite/schedule-maker.py
+++ Day 28 - SchedulesWebsite/schedule-maker.py
@@ -10,11 +10,32 @@ schedule = {
 
 yesses = ['y', 'yes']
 
+
+def get_class_info(day):
+    print()
+    class_name = input('What is the name of the class? ')
+    start = input('When does the class start? (0h00) ')
+    duration = int(input('How long is the class? (in minutes ex: 180) '))
+    room = input('What is the room number? ')
+    schedule[str(day)][class_name] = {
+        "start": start,
+        "duration": duration,
+        "room": room
+    }
+
+
 name = input('What is your name? ')
 
 weekends = input("Do you have any classes on weekends? [y/N] ")
 if weekends.lower() in yesses:
-    print('soy lazy rn, sooo.. just continue and update it in the schedules.json (also I\'m sorry for you)') # TODO: Add functionality
+    weekend_days = ["Saturday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"]
+    for day in [0, 6]:
+        is_class = input(f'Do you have class on {weekend_days[day]}? [Y/n] ')
+        if is_class.lower() in yesses or is_class == '':
+            class_num = int(input('How many classes do you have that day? '))
+            for i in range(class_num):
+                get_class_info(day)
+
 
 days=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
@@ -23,15 +44,7 @@ for day in range(1,6):
     if is_class.lower() in yesses or is_class == '':
         class_num = int(input('How many classes do you have that day? '))
         for i in range(class_num):
-            print()
-            class_name = input('What is the name of the class? ')
-            start = input('When does the class start? (0h00) ')
-            duration = int(input('How long is the class? (in minutes ex: 180) '))
-            room = input('What is the room number? ')
-            schedule[str(day)][class_name] = {
-                "start": start,
-                "duration": duration,
-                "room": room
-            }
+            get_class_info(day-1)
+
 print(f'''"{name}": {str(schedule).replace("'", '"')}''')
 print('Add this to schedules.json')
